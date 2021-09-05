aeaf275cbd195bc3e7f8fb8271ff2b096f27c3f1
James Lucas <james@lucas.net.au>
2021-09-06 08:49:33 +1000

    Tidy up the scheduler test script

--- tests/scheduler.php
+++ tests/scheduler.php
@@ -13,22 +13,6 @@ ini_set('display_errors', "on");
 
 include __DIR__ . "/../vendor/autoload.php";
 
-function handle_exception($exception) {
-    $ex_class = get_class($exception);
-    $message = <<<EOM
-Exception : {$ex_class} {$exception->getCode()}
-File: {$exception->getFile()}
-Line: {$exception->getLine()}
-
-Message: {$exception->getMessage()}
-Trace: 
-{$exception->getTraceAsString()}
-EOM;
-    fwrite(STDERR, "$message\n");
-}
-
-set_exception_handler("handle_exception");
-
 $rules = [
     MatchAnyRule::class,
     MatchOneRule::class,
@@ -40,19 +24,39 @@ $scheduler = new class($rules) extends Scheduler {
     const MEMORY_PRESSURE_HIGH_WATERMARK = 75;
     const MEMORY_PRESSURE_LOW_WATERMARK = 50;
     //const RUNNING_ACTION_LIMIT_HIGH_WATERMARK = 200000;
+
+    public function __construct(array $rules)
+    {
+        set_exception_handler([$this, "handle_exception"]);
+
+        parent::__construct($rules);
+        $this->register_input_process('test_data_stream_1', php_cmd(__DIR__ . "/test_input_1.php"));
+        $this->register_input_process('test_data_stream_2', php_cmd(__DIR__ . "/test_input_2.php"));
+
+        if (file_exists('/tmp/php_ec-scheduler_test_logs.txt')) {
+            unlink('/tmp/php_ec-scheduler_test_logs.txt');
+        }
+        $this->register_action('log', php_cmd(__DIR__ . "/log.php"), null, false, [
+            'LOG_FILENAME' => '/tmp/php_ec-scheduler_test_logs.txt',
+        ]);
+
+        $this->setSavefileName("/tmp/php_ec-scheduler_test.state");
+        $this->setSaveStateInterval(1);
+    }
+
+    function handle_exception($exception) {
+        $ex_class = get_class($exception);
+        $message = <<<EOM
+Exception : {$ex_class} {$exception->getCode()}
+File: {$exception->getFile()}
+Line: {$exception->getLine()}
+
+Message: {$exception->getMessage()}
+Trace: 
+{$exception->getTraceAsString()}
+EOM;
+        fwrite(STDERR, "$message\n");
+    }
 };
-
-$scheduler->register_input_process('test_data_stream_1', php_cmd(__DIR__ . "/test_input_1.php"), null, []);
-$scheduler->register_input_process('test_data_stream_2', php_cmd(__DIR__ . "/test_input_2.php"), null, []);
-
-if (file_exists('/tmp/php_ec-scheduler_test_logs.txt')) {
-    unlink('/tmp/php_ec-scheduler_test_logs.txt');
-}
-$scheduler->register_action('log', php_cmd(__DIR__ . "/log.php"), null, false, [
-    'LOG_FILENAME' => '/tmp/php_ec-scheduler_test_logs.txt',
-]);
-
-$scheduler->setSavefileName("/tmp/php_ec-scheduler_test.state");
-$scheduler->setSaveStateInterval(1);
 
 $scheduler->run();
