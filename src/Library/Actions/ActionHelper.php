64bf0e88d9ccf49c4df1935e88bdbed7f3209b2d
James Lucas <james@lucas.net.au>
2021-10-01 11:52:29 +1000

    Add a Helper class for building Actions. This takes care of setting up the STDIN and STDOUT streams with NDJSON encoding/decoding, RPC decoding and Signal handling. The class emits two events "run" and "shutdown".
    Actions should run their action on the run event, and shutdown and signal a stop() once complete when receiving the shutdown event.

---
+++ src/Library/Actions/ActionHelper.php
@@ -0,0 +1,84 @@
+<?php declare(strict_types=1);
+
+namespace EdgeTelemetrics\EventCorrelation\Library\Actions;
+
+use Clue\React\NDJson\Encoder;
+use EdgeTelemetrics\EventCorrelation\Scheduler;
+use EdgeTelemetrics\JSON_RPC\Notification as JsonRpcNotification;
+use EdgeTelemetrics\JSON_RPC\React\Decoder;
+use EdgeTelemetrics\JSON_RPC\Response as JsonRpcResponse;
+use EdgeTelemetrics\JSON_RPC\Request as JsonRpcRequest;
+use EdgeTelemetrics\JSON_RPC\Error as JsonRpcError;
+use Evenement\EventEmitter;
+use React\EventLoop\Loop;
+use React\EventLoop\LoopInterface;
+use React\Stream\ReadableStreamInterface;
+use React\Stream\WritableStreamInterface;
+
+use EdgeTelemetrics\JSON_RPC\RpcMessageInterface;
+use Psr\Log\LogLevel;
+use React\Stream\ReadableResourceStream;
+use React\Stream\WritableResourceStream;
+
+use function EdgeTelemetrics\EventCorrelation\disableOutputBuffering;
+use function EdgeTelemetrics\EventCorrelation\rpcLogMessage;
+
+/**
+ * Class Action Helper
+ * @package EdgeTelemetrics\EventCorrelation
+ */
+class ActionHelper extends EventEmitter {
+
+    protected LoopInterface $loop;
+
+    protected WritableStreamInterface $output;
+
+    protected ReadableStreamInterface $input;
+
+    const ACTION_EXECUTE = 'run';
+
+    const ACTION_SHUTDOWN = 'shutdown';
+
+    public function __construct() {
+        $this->loop = Loop::get();
+        disableOutputBuffering();
+
+        $this->input = new Decoder( new ReadableResourceStream(STDIN));
+        $this->output = new Encoder( new WritableResourceStream(STDOUT));
+
+        $this->input->on('data', function(JsonRpcNotification $rpc) {
+            if (Scheduler::ACTION_RUN_METHOD === $rpc->getMethod()) {
+                $this->emit(self::ACTION_EXECUTE, [$rpc]);
+            } else {
+                if ($rpc instanceof JsonRpcRequest) {
+                    $error = new JsonRpcError(JsonRpcError::METHOD_NOT_FOUND,
+                        JsonRpcError::ERROR_MSG[JsonRpcError::METHOD_NOT_FOUND]);
+                    $response = JsonRpcResponse::createFromRequest($rpc, $error);
+                    $this->output->write($response);
+                }
+            }
+        });
+
+        /** Signal Handlers */
+        $this->loop->addSignal(SIGINT, function() {
+            $this->write(rpcLogMessage(LogLevel::DEBUG, "received SIGINT, flushing..."));
+            $this->emit(self::ACTION_SHUTDOWN);
+        });
+        $this->loop->addSignal(SIGTERM, function() {
+            $this->write(rpcLogMessage(LogLevel::DEBUG, "received SIGTERM, flushing..."));
+            $this->emit(self::ACTION_SHUTDOWN);
+        });
+    }
+
+    public function write(RpcMessageInterface $rpc) {
+        $this->output->write($rpc);
+    }
+
+    public function run() {
+        $this->loop->run();
+    }
+
+    public function stop() {
+        $this->loop->stop();
+    }
+}
