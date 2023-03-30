6947577f8c5c2065716f17f48fbe4592ab19ee34
James Lucas <james@lucas.net.au>
2023-03-30 14:30:05 +1100

    Split out the composer loading preamble to a separate file for reuse

---
+++ bin/composer.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+$dir = __DIR__.'/..';
+
+if (!file_exists($dir.'/autoload.php')) {
+    $dir = __DIR__.'/../vendor';
+}
+
+if (!file_exists($dir.'/autoload.php')) {
+    $dir = __DIR__.'/../../..';
+}
+
+if (!file_exists($dir.'/autoload.php')) {
+    echo 'Autoload not found.';
+    exit(1);
+}
+
+require $dir.'/autoload.php';
--- bin/source_prepend.php
+++ bin/source_prepend.php
@@ -2,21 +2,6 @@
 
 use function EdgeTelemetrics\EventCorrelation\initialiseSourceProcess;
 
-$dir = __DIR__.'/..';
-
-if (!file_exists($dir.'/autoload.php')) {
-    $dir = __DIR__.'/../vendor';
-}
-
-if (!file_exists($dir.'/autoload.php')) {
-    $dir = __DIR__.'/../../..';
-}
-
-if (!file_exists($dir.'/autoload.php')) {
-    echo 'Autoload not found.';
-    exit(1);
-}
-
-require $dir.'/autoload.php';
+require __DIR__ . '/composer.php';
 
 initialiseSourceProcess(true);
