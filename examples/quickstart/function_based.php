44656dd5f4058f9b866e6fc6f2b74a91c5d1d651
James Lucas <james@lucas.net.au>
2025-03-06 12:26:18 +1100

    Create a SourceFunction class called GeneratorSource designed to wrap Generator objects to create quick inputs. function_based example updated to use a Generator to count to 10.

--- examples/quickstart/function_based.php
+++ examples/quickstart/function_based.php
@@ -1,69 +1,41 @@
 <?php declare(strict_types=1);
 
 use Bref\Logger\StderrLogger;
+use EdgeTelemetrics\EventCorrelation\Action;
 use EdgeTelemetrics\EventCorrelation\Event;
+use EdgeTelemetrics\EventCorrelation\Library\Source\GeneratorSource;
 use EdgeTelemetrics\EventCorrelation\Rule;
 use EdgeTelemetrics\EventCorrelation\Scheduler;
 use Psr\Log\LogLevel;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-class SendCountToStdOut extends Rule\MatchSingle {
+$sendCountToStdOut = new class extends Rule\MatchSingle {
     const EVENTS = [['SampleValueEvent']];
 
-    public function onComplete(): void
-    {
-        $this->emit('data', [New \EdgeTelemetrics\EventCorrelation\Action('echo', ['value' => $this->getFirstEvent()->value])]);
+    public function onComplete(): void {
+        $this->emit('data', [New Action('echo', ['value' => $this->getFirstEvent()->value])]);
     }
-}
+};
 
 $rules = [
-    SendCountToStdOut::class,
+    $sendCountToStdOut::class,
 ];
 
-$scheduler = new \EdgeTelemetrics\EventCorrelation\Scheduler($rules);
+$scheduler = new Scheduler($rules);
 $scheduler->setLogger(new StderrLogger(LogLevel::DEBUG));
 
 $scheduler->register_action('echo', function($vars) {
     echo 'Next Value: ' . $vars['value'] . PHP_EOL;
 });
 
-$numberGenClass = new class() extends Scheduler\SourceFunction {
-    protected \React\EventLoop\TimerInterface $timer;
-
-    function functionStart(): void
-    {
-        $this->timer = $this->loop->addPeriodicTimer(1.0, function () {
-            static $count = 1;
-            try {
-                $event = new Event(['event' => 'SampleValueEvent', 'value' => $count++]);
-                $this->emit('data', [$event]);
-
-                if ($count > 10) {
-                    $this->exit();
-                }
-            } catch (Throwable $exception) {
-                $this->emit('error', [$exception]);
-                $this->exit(255);
-                return;
-            }
-        });
-    }
-
-    function exit(int $code = 0): void
-    {
-        $this->running = false;
-        $this->loop->cancelTimer($this->timer);
-        unset($this->timer);
-        $this->emit('exit', [$code]);
-    }
-
-    function functionStop(): void
-    {
-        $this->exit();
+function countToTen() : Generator {
+    for($i = 1;  $i <= 10; $i++) {
+        yield new Event(['event' => 'SampleValueEvent', 'value' => $i]);
     }
 };
+$numberGenClass = new GeneratorSource(countToTen(), 1.0);
 
-$scheduler->register_input_process('generator', $numberGenClass, null, [], false);
+$scheduler->register_input_process('generator', $numberGenClass);
 
 $scheduler->run();
---
+++ src/Library/Source/GeneratorSource.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace EdgeTelemetrics\EventCorrelation\Library\Source;
+
+use EdgeTelemetrics\EventCorrelation\Event;
+use EdgeTelemetrics\EventCorrelation\Scheduler\SourceFunction;
+use Generator;
+use React\EventLoop\TimerInterface;
+use RuntimeException;
+
+class GeneratorSource extends SourceFunction {
+    protected TimerInterface $timer;
+
+    public function __construct(private readonly Generator $generator, private readonly float|int $delay = 0) {
+        parent::__construct();
+        if ($this->delay < 0) {
+            throw new RuntimeException('Delay must be greater than 0');
+        }
+    }
+
+    public function functionStart(): void {
+        $this->loop->futureTick($this->tick(...));
+    }
+
+    private function tick(): void {
+        if ($this->generator->valid()) {
+            $event = $this->generator->current();
+            if ($event instanceof Event) {
+                $this->emit('data', [$event]);
+            }
+
+            $this->timer = $this->loop->addTimer($this->delay, function() {
+                try {
+                    $this->generator->next();
+                    $this->tick();
+                } catch (\Throwable $e) {
+                    $this->logger->critical('Generator threw exception', ['exception' => $e]);
+                }
+            });
+        } else {
+            $this->exit();
+        }
+    }
+
+    public function exit(int $code = 0): void {
+        $this->running = false;
+        if (isset($this->timer)) {
+            $this->loop->cancelTimer($this->timer);
+            unset($this->timer);
+        }
+        $this->emit('exit', [$code]);
+    }
+
+    public function functionStop(): void {
+        $this->exit();
+    }
+}
